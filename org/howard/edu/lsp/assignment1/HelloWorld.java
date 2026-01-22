fdd574e71b71aeea9312fc8cd7045051b30c625a
ericaokeh <157079075+ericaokeh@users.noreply.github.com>
2026-01-22 11:51:46 -0500

    Initial commit

---
+++ org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+ * Erica Okeh
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
+Wh
